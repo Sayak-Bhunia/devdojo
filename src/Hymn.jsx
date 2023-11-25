3b1c7bd2fcf1976e50a7e9bad9d547babdc77ffd
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-11-25 09:33:26 +0530

    Update Hymn.jsx

--- src/Hymn.jsx
+++ src/Hymn.jsx
@@ -5,7 +5,7 @@ const Hymn = () => (
   <section className="bg7">
     <div className="container7">
       <div className="column-left7">
-        <p>💠The Team</p>
+        <p>💠 The Team</p>
         <h1>meet the sensei behind dev dojo</h1>
       </div>
     </div>
