12d01336b9f98b82f71c64012b9204622a4d7a3d
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-12-24 18:01:49 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => (
   <nav>
     <input type="checkbox" id="check" />
     <label htmlFor="check" className="checkbtn">
-      <a href="https://github.com/devdojo14756" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/The-Dev-Dojo/" target="_blank" rel="noopener noreferrer">
         <i className='bx bxl-github bx-md'></i>
       </a>
       <a href="https://www.linkedin.com/company/99019744/admin/feed/posts/" target="_blank" rel="noopener noreferrer">
