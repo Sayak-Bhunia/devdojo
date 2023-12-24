4295bd7fd2b7b0a93092f7fe821be333131d04ca
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2023-12-24 18:01:00 +0530

    Update Middle.jsx

--- src/Middle.jsx
+++ src/Middle.jsx
@@ -8,7 +8,7 @@ const Middle = () => (
       <div className="column-left3">
         <p>🛸 Collaborate with Us</p>
         <h1>organize an event with dev dojo</h1>
-        <a href="https://twitter.com/DevDojo186759"><button class="cssbuttons-io">
+        <a href="https://twitter.com/DevDojo_"><button class="cssbuttons-io">
             <span>
             <svg viewBox="0 0 24 24">
             <path fill="currentColor" d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"></path>
